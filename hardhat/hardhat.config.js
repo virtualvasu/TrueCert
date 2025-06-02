0366236829d87d515f801d9a4c3b4fdff8541b17
virtualvasu <virtualvasu624@gmail.com>
2025-06-02 14:04:32 +0530

    organising code for better developer

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,12 +1,13 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-
+require("dotenv").config(); 
 require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/EYRFiITV94hgOi9hjRQ14OxPTNPDFgtw", // Replace with your Alchemy URL
-      accounts: ["2e33f1db5024aae3edead1e5b1edf6bd8e14937533ec8ea2e980bf8cce28f46a"], // Replace with your private key
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
